cb6d55ada4fb4efd35b84e5a685475ed8d22f5fb
ltmervin123 <ltmervin123@gmail.com>
2024-09-06 19:00:36 +0800

    first commit

---
+++ src/main/java/com/alvindev/Amazon/Project/Back/End/AmazonProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.alvindev.Amazon.Project.Back.End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvindev/Amazon/Project/Back/End/AmazonProjectBackEndApplicationTests.java
@@ -0,0 +1,12 @@
+package com.alvindev.Amazon.Project.Back.End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
